30eafb3b9b5d7e8996e117c0c6fbf138df32e3a3
student <student@DESKTOP-5POI41D>
2025-12-02 16:01:01 +0100

    Add project files.

---
+++ lab 8_PP/lab 8_PP.cpp
@@ -0,0 +1,21 @@
+// lab 8_PP.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //Zadanie 2.1
+    const int size = 3;
+    int tab[size] = { 1,2,3 };
+    for (int i = 0; i < size; i++) {
+        cout << "Indeks: " << i << " Wartoœæ: " << tab[i] << endl;
+    }
+    int* tab2[size];
+    for (int i = 0; i < size; i++) {
+        tab2[i] = &tab[i];
+        cout << "Indeks: " << i << " Wartoœæ: " << tab2[i] << endl;
+    }
+}
+
